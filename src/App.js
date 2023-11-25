ef42cbdac0a196c5bffff2a395405087ec64944b
sumitsalve01 <139698732+sumitsalve01@users.noreply.github.com>
2023-11-25 20:19:32 +0530

    Final Commit
    Final code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherApp from './components/WeatherApp/WeatherApp.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
